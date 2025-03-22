d87f53eca336d618009ad46931c6978a5b735bb4
James Westover <james@neherdata.com>
2025-03-22 12:48:25 -0700

    Fixing some logging

--- cron.sh
+++ cron.sh
@@ -3,10 +3,16 @@
 # Setup pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+eval "$("$PYENV_ROOT/bin/pyenv" init -)"
 
 # Move to project directory
 cd "$HOME/porkbun_updater" || exit 1
 
-# Run your command with pdm
-pdm run python porkbun_updater/main.py set-dns-record-by-type --record-type A
+# Timestamp
+echo "[$(date)] Starting DNS update" >> "$HOME/porkbun_updater/cron.log"
+
+# Run your command with pdm, log stdout and stderr
+pdm run python porkbun_updater/main.py set-dns-record-by-type --record-type A >> "$HOME/porkbun_updater/cron.log" 2>&1
+
+# Completion marker
+echo "[$(date)] DNS update finished" >> "$HOME/porkbun_updater/cron.log"
