aa4c11ab9b73ac1d8e39c168549bab3c64bf63eb
James Westover <james@neherdata.com>
2025-03-22 15:59:02 -0700

    trying some more

--- cron.sh
+++ cron.sh
@@ -11,9 +11,6 @@ export LC_ALL=C
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"
 
-# Initialize pyenv
-eval "$("$PYENV_ROOT/bin/pyenv" init -)"
-
 # Move to project dir
 cd "$HOME/porkbun_updater" || exit 1
 
