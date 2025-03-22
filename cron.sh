86b16c1c71d7e782161241b1f7bb3df18164d8b5
James Westover <james@neherdata.com>
2025-03-22 14:04:38 -0700

    more logging

--- cron.sh
+++ cron.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 set +x
+
 # Setup pyenv
 export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$HOME/.local/bin:$PATH"
 eval "$("$PYENV_ROOT/bin/pyenv" init -)"
 
 # Move to project directory
-cd "$HOME/porkbun_updater" || exit 1
+cd "$HOME/porkbun_updater" || {
+  echo "[$(date)] Failed to cd into porkbun_updater" >> "$HOME/porkbun_updater/cron.log"
+  exit 1
+}
 
-# Timestamp
+# Timestamp before running
 echo "[$(date)] Starting DNS update" >> "$HOME/porkbun_updater/cron.log"
 
-# Run your command with pdm, log stdout and stderr
+# Use full path to pdm and log both stdout and stderr
 pdm run python porkbun_updater/main.py set-dns-record-by-type --record-type A >> "$HOME/porkbun_updater/cron.log" 2>&1
 
-# Completion marker
+# Timestamp after running
 echo "[$(date)] DNS update finished" >> "$HOME/porkbun_updater/cron.log"
